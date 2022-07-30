23d67cd3502fbbba8a88300b00fcec65b040e32c
Samuel Bryan <remotepilotsam@gmail.com>
2022-07-29 20:29:54 -0400

    Template created.  Need to add comments and figure out how to divide an int and round up.

--- 15b/15b.cpp
+++ 15b/15b.cpp
@@ -1,14 +1,22 @@
 //Samuel Bryan
 //CIS 1202 800
-//July 30th, 2022
+//July 29th, 2022
 
-using namespace std;
 #include <iostream>
+#include <cctype>
+using namespace std;
 
-template <typename H>
+template <typename H> //Template to divide all values
 H half(H number)
 {
-
+    if (isdigit(number))
+    {
+        return number / 2;
+    }
+    else
+    {
+        return number / 2;
+    }
 }
 
 int main()
@@ -16,9 +24,13 @@ int main()
     double a = 7.0;
     float b = 5.0f;
     int c = 3;
-
-    cout << half(a) << endl;
-    cout << half(b) << endl;
-    cout << half(c) << endl;
+    
+    cout << "*******************************************\n\n";
+    cout << half(a) << endl; //half of double value
+    cout << half(b) << endl; //half of float value
+    cout << half(c) << endl; //half of int value
+    cout << "\n*******************************************\n\n\n";
+    system("pause");
+    return 0;
 }
 
