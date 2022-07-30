651353c9aa00291832a89f4a5b4e786fcd5d4f16
Samuel Bryan <remotepilotsam@gmail.com>
2022-07-29 21:46:49 -0400

    Combined float and double to one template

--- 15b/15b.cpp
+++ 15b/15b.cpp
@@ -8,32 +8,23 @@
 using namespace std;
 
 template <typename H> 
-H halfD(H number1) //double template
+H halfDF(H number1) //double and float template
 {
     return number1 / 2; //round function rounds quotient of double 
 }
-
-template <typename H>
-H halfF(H number2) //float tempalte
-{
-    return number2 / 2; //round function rounds quotient of float
-}
-
 template <typename H>
 H halfI(H number3) //int tempalte
 {
     return number3 / 2; //round function rounds quotient of int
 }
-
 int main()
 {
     double a = 7.0;
     float b = 5.0f; //f to assign float
     int c = 3;
-
     cout << "*******************************************\n\n";
-    cout << halfD(a) << endl; //half of double value
-    cout << halfF(b) << endl; //half of float value
+    cout << halfDF(a) << endl; //half of double value
+    cout << halfDF(b) << endl; //half of float value
     cout << round(halfI(round(c))) << endl; /*half of int value
     ^^The initial result of 3 / 2 was 1.  With the round inside halfI(), it was 1.5 (the correct answer  
     With the round outside halfI(), it is 2, rounded up as the assignment wanted */
@@ -41,4 +32,3 @@ int main()
     system("pause");
     return 0;
 }
-
