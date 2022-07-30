7ec2cc16e45841340767a6a7664d10eeec403be6
Samuel Bryan <remotepilotsam@gmail.com>
2022-07-29 20:08:42 -0400

    Add project files.

---
+++ 15b/15b.cpp
@@ -0,0 +1,24 @@
+//Samuel Bryan
+//CIS 1202 800
+//July 30th, 2022
+
+using namespace std;
+#include <iostream>
+
+template <typename H>
+H half(H number)
+{
+
+}
+
+int main()
+{
+    double a = 7.0;
+    float b = 5.0f;
+    int c = 3;
+
+    cout << half(a) << endl;
+    cout << half(b) << endl;
+    cout << half(c) << endl;
+}
+
